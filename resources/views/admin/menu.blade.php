ce34ed58e75488228b575f13ed9b2871ac0ec423
kamilova-bonya <bkamilova823@gmail.com>
2025-06-20 19:23:13 +0300

    update menu-admin

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -1,2 +1,3 @@
 <a href="{{ route('home') }}">Главная</a>
-<a href="{{ route('admin.posts.index') }}">Посты</a><br>
+<a href="{{ route('admin.posts.index') }}">Посты</a>
+<a href="{{ route('admin.categories.index') }}">Категории</a><br>
