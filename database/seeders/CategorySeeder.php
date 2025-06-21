c7a626d261bfb64cce8f50190ac29efbaea2280d
kamilova-bonya <bkamilova823@gmail.com>
2025-06-21 17:01:29 +0300

    update categoryseeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Category;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
@@ -15,12 +15,6 @@ class CategorySeeder extends Seeder
             ['name' => 'Политика'],
             ['name' => 'Спорт']
         ];
-
-        foreach ($categories as $category) {
-            Category::updateOrCreate(
-                ['name' => $category['name']],
-                $category
-            );
-        }
+        DB::table('categories')->insert($categories);
     }
 }
