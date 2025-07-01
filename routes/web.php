fc146f3880282de6e432e49a5e456c50b8d1be91
kamilova-bonya <bkamilova823@gmail.com>
2025-07-01 18:36:45 +0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
-
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
